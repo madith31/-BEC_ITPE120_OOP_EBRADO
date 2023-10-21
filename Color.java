81b66e10fbec6ca6c8f6f250bb77668b23ebf43b
madith31 <148523506+madith31@users.noreply.github.com>
2023-10-22 00:15:14 +0800

    Add files via upload

---
+++ Color.java
@@ -0,0 +1,38 @@
+    public enum Color {
+        RED(255, 0, 0),
+        GREEN(0, 255, 0),
+        BLUE(0, 0, 255),
+        YELLOW(255, 255, 0),
+        MAGENTA(255, 0, 255),
+        CYAN(0, 255, 255),
+        BLACK(0, 0, 0),
+        WHITE(255, 255, 255);
+
+        private int red;
+        private int green;
+        private int blue;
+
+        Color(int red, int green, int blue) {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+
+        public int getRed() {
+            return red;
+        }
+
+        public int getGreen() {
+            return green;
+        }
+
+        public int getBlue() {
+            return blue;
+        }
+
+        public String getRGBValues() {
+            return "RGB(" + red + ", " + green + ", " + blue + ")";
+        }
+    }
+
+
